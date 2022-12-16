16ce3bb3d3dc2e6740695eda9a3282fe69375a5e
morsa <voxfoot@gmail.com>
2022-12-16 23:35:42 +0500

    add light for header to-do

--- src/widgets/Todo/components/Information.jsx
+++ src/widgets/Todo/components/Information.jsx
@@ -2,14 +2,15 @@ import React, { useState, useEffect } from 'react';
 import cn from 'classnames';
 import getDateAndTime, { getHour } from '../../../utils/getDateAndTime.js';
 
+import { ReactComponent as Light } from '../../../img/light.svg';
+
 const Information = () => {
 	const [state, setState] = useState(getDateAndTime());
-	const classes = cn('header-container', {
+
+	const classesHeader = cn('header-container', {
 		morning: getHour() >= 7 && getHour() <= 18,
 		evening: getHour() > 18 || getHour() < 7,
 	});
-
-	console.log(getHour());
 
 	useEffect(() => {
 		const interval = setInterval(() => {
@@ -20,7 +21,10 @@ const Information = () => {
 	}, []);
 
 	return (
-		<div className={classes}>
+		<div className={classesHeader}>
+			<div className="header__light">
+				<Light className="light shining" />
+			</div>
 			<div className="header-information">
 				<div className="today pt-pb-5">Сегодня</div>
 				<div className="today__date pt-pb-5">{state.currentDate}</div>
