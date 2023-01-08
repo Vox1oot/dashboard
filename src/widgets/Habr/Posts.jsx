283f52333b2c5913cff701638e0e901266e96f06
morsa <voxfoot@gmail.com>
2023-01-08 19:14:38 +0500

    feature: implement habr posts

--- src/widgets/Habr/Posts.jsx
+++ src/widgets/Habr/Posts.jsx
@@ -4,9 +4,25 @@ const Posts = ({ posts }) => (
 	<div className="habr-posts">
 		{ posts.map((item, index) => (
 			// eslint-disable-next-line react/no-array-index-key
-			<div className="posts-item" key={index}>
+			<div className="posts-item post-item_hover" key={index}>
 				<div className="posts-item__title">
 					{item.title}
+				</div>
+				<div className="posts-item__main">
+					<div className="posts-item__description">
+						{item.description
+							.replaceAll(/<[^>]*>/g, '')
+							.replace('Читать далее', '')}
+					</div>
+					<div className="posts-item__link">
+						<a
+							href={item.url}
+							target="_blank"
+							rel="noreferrer"
+						>
+							Читать далее
+						</a>
+					</div>
 				</div>
 			</div>
 		)) }
